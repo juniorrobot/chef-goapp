a06bbe305aff355457df8d7e7f95cfa6bf25d36e
Geoff Hayes <hayesgm@gmail.com>
2014-01-13 00:08:23 -0800

    make sure we have proper permissions on shared dir

--- definitions/goapp_deploy_dir.rb
+++ definitions/goapp_deploy_dir.rb
@@ -1,7 +1,7 @@
 define :goapp_deploy_dir do
   # create shared/ directory structure
-  ['log','config','system','pids','scripts','sockets'].each do |dir_name|
-    directory "#{params[:path]}/shared/#{dir_name}" do
+  ['', '/log','/config','/system','/pids','/scripts','/sockets'].each do |dir_name|
+    directory "#{params[:path]}/shared#{dir_name}" do
       group params[:group]
       owner params[:user]
       mode 0770
